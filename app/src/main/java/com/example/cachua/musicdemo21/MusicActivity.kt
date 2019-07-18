5108fa82f3d44d3bdeda7e1a3d697cdef9e6b532
Hien Oc <Hiendochelsea@gmail.com>
2019-07-18 10:33:11 +0700

    init

---
+++ app/src/main/java/com/example/cachua/musicdemo21/MusicActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cachua.musicdemo21
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MusicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music)
+    }
+}
